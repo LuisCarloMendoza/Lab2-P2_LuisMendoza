ca1c840478e6281781526db087b126c8d20f9dde
LuisCarloMendoza <luiscarlograna2@gmail.com>
2023-01-27 13:42:14 -0600

    1. Cree el proyecto con las clases necesarias

---
+++ src/lab/pkg2p2_luismendoza/Lab2P2_LuisMendoza.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab.pkg2p2_luismendoza;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Lab2P2_LuisMendoza {
+
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
